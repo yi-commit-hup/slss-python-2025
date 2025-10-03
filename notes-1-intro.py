3ec15614b9f5446f7d6ddbf02b320d8c5ea9d27b
Tom Zhang <mton9706@gmail.com>
2025-10-03 09:44:38 -0700

    First commit - added notes

---
+++ notes-1-intro.py
@@ -0,0 +1,28 @@
+# Notes - introduction
+# 16 September
+# Tom Z
+
+# Create our own Chatbot
+
+# 1. Greet the user with a predetermined statement
+greeting = "Hello! I am a chatbot"
+
+# 2. Print statement
+print(greeting)
+#1a
+# introduce the bot
+print("My name is MEgpt")
+print("I am not like the other chatbots")
+print("Other chatbots can generate content, I can say pre-exsiting codes.")
+
+# 3. Wow the user with some math
+print("I bet you don't know what 888878*888888 is")
+print(" I can do it")
+print(f"It is actually {888878*888888}")
+user-name = input("So... what's your name?")
+print(f"It's nice to meet you, {user name}")
+print(f"Good  want knoe more about you {user name}")
+fav_book = input ("What is your favourite book.")
+print ("favourite book, nice")
+print(f"What a fun time chatting with you {user name}")
+print ("bye")
